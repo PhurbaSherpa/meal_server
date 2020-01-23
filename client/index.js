79739164799669ca68ef549c8e00e0b080ef1dfc
Phurba Sherpa <srpphurba@gmail.com>
2020-01-23 14:30:02 -0500

    updated config files

--- client/index.js
+++ client/index.js
@@ -1,13 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
-import App from './app'
-
-// establishes socket connection
-import './socket'
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { Router } from "react-router-dom";
+import history from "./history";
+import store from "./store";
+import App from "./app";
+import "./socket";
 
 ReactDOM.render(
   <Provider store={store}>
@@ -15,5 +13,5 @@ ReactDOM.render(
       <App />
     </Router>
   </Provider>,
-  document.getElementById('app')
-)
+  document.getElementById("app")
+);
