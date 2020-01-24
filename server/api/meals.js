9297cff9eac42eddbb3a5bed4382180b2f488ed2
Phurba Sherpa <srpphurba@gmail.com>
2020-01-24 14:26:29 -0500

    added delete route/ updated old routes

--- server/api/meals.js
+++ server/api/meals.js
@@ -2,15 +2,49 @@ const router = require("express").Router();
 const { SingleFood } = require("../db/models");
 module.exports = router;
 
-router.get("/:mealType/:date", async (req, res, next) => {
+// router.get("/:mealType/:date", async (req, res, next) => {
+//   try {
+//     const foods = await SingleFood.findAll({
+//       where: { mealType: req.params.mealType, date: req.params.date }
+//     });
+//     if (!foods) res.sendStatus(204);
+//     else res.json(foods);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
+
+router.get("/:date", async (req, res, next) => {
   try {
-    const foods = await SingleFood.findAll({
-      where: { mealType: req.params.mealType, date: req.params.date }
+    const allfoods = await SingleFood.findAll({
+      where: { date: req.params.date }
     });
-    if (!foods) res.sendStatus(204);
-    else res.json(foods);
-  } catch (err) {
-    next(err);
+    if (!allfoods) res.sendStatus(204);
+    else res.json(allfoods);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:barcodeId/:date/:mealType", async (req, res, next) => {
+  try {
+    const toBeDestroyed = await SingleFood.findOne({
+      where: {
+        barcodeId: req.params.barcodeId,
+        date: req.params.date,
+        mealType: req.params.mealType
+      }
+    });
+    console.log(toBeDestroyed);
+
+    if (!toBeDestroyed) res.sendStatus(204);
+    else {
+      console.log(toBeDestroyed);
+      await toBeDestroyed.destroy();
+      res.json(toBeDestroyed);
+    }
+  } catch (error) {
+    console.log("delete error", error);
   }
 });
 
